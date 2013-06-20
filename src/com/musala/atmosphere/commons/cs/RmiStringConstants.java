fae7cc5a7e06201d69cef7f19bf0b1b08a74e809
georgi.gaydarov <georgi.gaydarov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-06-20 08:54:29 +0000

    Resubmitting changes that failed to commit in [8332].
    git-svn-id: https://trac.musala.com/ARDC/repo@8333 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ src/com/musala/atmosphere/commons/cs/RmiStringConstants.java
@@ -0,0 +1,22 @@
+package com.musala.atmosphere.commons.cs;
+
+public enum RmiStringConstants
+{
+	/**
+	 * String, under which the {@link PoolManager PoolManager} class is published in the Agent's RMI registry
+	 */
+	POOL_MANAGER("PoolManager");
+
+	private String value;
+
+	private RmiStringConstants(String value)
+	{
+		this.value = value;
+	}
+
+	@Override
+	public String toString()
+	{
+		return value;
+	}
+}
