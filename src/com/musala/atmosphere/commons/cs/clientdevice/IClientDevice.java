cb7bbbf7a9d0e9a04af092802767021ba723cd29
yordan.petrov <yordan.petrov@34708651-a5da-c34d-b20c-10ddab38317d>
2013-10-07 13:31:25 +0000

    Fix Ant.
    git-svn-id: https://trac.musala.com/ARDC/repo@8777 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/cs/clientdevice/IClientDevice.java
+++ src/com/musala/atmosphere/commons/cs/clientdevice/IClientDevice.java
@@ -25,13 +25,6 @@ import com.musala.atmosphere.commons.util.Pair;
 public interface IClientDevice extends Remote
 {
 	/**
-	 * Validates whether a communication to the ATMOSPHERE service is available.
-	 * 
-	 * @return - true if communication is available; false otherwise.
-	 */
-	public boolean validateServiceCommunication(long invocationPasskey) throws RemoteException, InvalidPasskeyException;
-
-	/**
 	 * Gets the amount of usable RAM of the user's device.
 	 * 
 	 * @param invocationPasskey
