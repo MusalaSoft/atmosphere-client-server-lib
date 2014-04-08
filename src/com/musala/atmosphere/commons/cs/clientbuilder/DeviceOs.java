3820af66e1cf59d4c7f0a892affb34117babcd1b
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-04-08 10:40:27 +0000

    git-svn-id: https://trac.musala.com/ARDC/repo@9861 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceOs.java
+++ src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceOs.java
@@ -1,46 +1,52 @@
 package com.musala.atmosphere.commons.cs.clientbuilder;
 
 /**
- * This enumeration contains all possible values for devices' OS. For now there are only three options:
- * <i>JELLY_BEAN</i> ( Android 4.1 ), <i>JELLY_BEAN_MR1</i> ( Android 4.2 ) and <i>NO_PREFERENCE</i> ( OS by default ).
+ * This enumeration contains all possible values for devices' OS. For now there are six options.
  * 
- * @author vladimir.vladimirov
+ * @author yavor.stankov
  * 
  */
 
-public enum DeviceOs
-{
-	/**
-	 * <i><b>Android 4.1</b></i>
-	 */
-	JELLY_BEAN_4_1("4.1"),
+public enum DeviceOs {
+    /**
+     * <i><b>Android 4.1</b></i>
+     */
+    JELLY_BEAN_4_1("4.1"),
 
-	/**
-	 * <i><b>Android 4.2.1</b></i>
-	 */
-	JELLY_BEAN_MR1_4_2_1("4.2.1"),
+    /**
+     * <i><b>Android 4.2.1</b></i>
+     */
+    JELLY_BEAN_MR1_4_2_1("4.2.1"),
 
-	/**
-	 * <i><b>Android 4.2.2</b></i>
-	 */
-	JELLY_BEAN_MR1_4_2_2("4.2.2"),
+    /**
+     * <i><b>Android 4.2.2</b></i>
+     */
+    JELLY_BEAN_MR1_4_2_2("4.2.2"),
 
-	/**
-	 * It means <i>"any available OS"</i>. OS by default when constructing new device.
-	 */
-	NO_PREFERENCE("no_preference");
+    /**
+     * <i><b>Android 4.3</b></i>
+     */
+    JELLY_BEAN_MR2_4_3("4.3"),
 
-	private String value;
+    /**
+     * <i><b>Android 4.4</b></i>
+     */
+    KITKAT_4_4("4.4"),
 
-	private DeviceOs(String deviceOs)
-	{
-		this.value = deviceOs;
-	}
+    /**
+     * It means <i>"any available OS"</i>. OS by default when constructing new device.
+     */
+    NO_PREFERENCE("no_preference");
 
-	@Override
-	public String toString()
-	{
-		return value;
-	}
+    private String value;
+
+    private DeviceOs(String deviceOs) {
+        this.value = deviceOs;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 
 }
