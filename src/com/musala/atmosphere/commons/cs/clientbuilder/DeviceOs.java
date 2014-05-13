d4ca180312b29c376aa508ff242612165a6e5f6d
filareta.yordanova <filareta.yordanova@34708651-a5da-c34d-b20c-10ddab38317d>
2014-05-13 08:11:08 +0000

    Updated DeviseOs enum. Add all KitKat versions of android os.
    [http://jenkins.musala.com/r/8156/ $8156] see #3071
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9961 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceOs.java
+++ src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceOs.java
@@ -1,69 +1,92 @@
 package com.musala.atmosphere.commons.cs.clientbuilder;
 
 /**
- * This enumeration contains all possible values for devices' OS. For now there are six options.
+ * This enumeration contains all possible values for devices' OS. For now there
+ * are nine options.
  * 
  * @author yavor.stankov
  * 
  */
 
 public enum DeviceOs {
-    /**
-     * <i><b>Android 4.1</b></i>
-     */
-    JELLY_BEAN_4_1("4.1"),
+	/**
+	 * <i><b>Android 4.1</b></i>
+	 */
+	JELLY_BEAN_4_1("4.1"),
 
-    /**
-     * <i><b>Android 4.2.1</b></i>
-     */
-    JELLY_BEAN_MR1_4_2_1("4.2.1"),
+	/**
+	 * <i><b>Android 4.2.1</b></i>
+	 */
+	JELLY_BEAN_MR1_4_2_1("4.2.1"),
 
-    /**
-     * <i><b>Android 4.2.2</b></i>
-     */
-    JELLY_BEAN_MR1_4_2_2("4.2.2"),
+	/**
+	 * <i><b>Android 4.2.2</b></i>
+	 */
+	JELLY_BEAN_MR1_4_2_2("4.2.2"),
 
-    /**
-     * <i><b>Android 4.3</b></i>
-     */
-    JELLY_BEAN_MR2_4_3("4.3"),
+	/**
+	 * <i><b>Android 4.3</b></i>
+	 */
+	JELLY_BEAN_MR2_4_3("4.3"),
 
-    /**
-     * <i><b>Android 4.4</b></i>
-     */
-    KITKAT_4_4("4.4"),
+	/**
+	 * <i><b>Android 4.4</b></i>
+	 */
+	KITKAT_4_4("4.4"),
 
-    /**
-     * It means <i>"any available OS"</i>. OS by default when constructing new device.
-     */
-    NO_PREFERENCE("no_preference");
+	/**
+	 * <i><b>Android 4.4.1</b></i>
+	 */
+	KITKAT_4_4_1("4.4.1"),
 
-    private String value;
+	/**
+	 * <i><b>Android 4.4.2</b></i>
+	 */
+	KITKAT_4_4_2("4.4.2"),
 
-    private DeviceOs(String deviceOs) {
-        this.value = deviceOs;
-    }
+	/**
+	 * <i><b>Android 4.4.2</b></i>
+	 */
+	KITKAT_4_4_3("4.4.3"),
 
-    @Override
-    public String toString() {
-        return value;
-    }
+	/**
+	 * It means <i>"any available OS"</i>. OS by default when constructing new
+	 * device.
+	 */
+	NO_PREFERENCE("no_preference");
 
-    public DeviceOs getDeviceOs(String os) {
-        switch (os) {
-            case "4.1":
-                return JELLY_BEAN_4_1;
-            case "4.2.1":
-                return JELLY_BEAN_MR1_4_2_1;
-            case "4.2.2":
-                return JELLY_BEAN_MR1_4_2_2;
-            case "4.3":
-                return JELLY_BEAN_MR2_4_3;
-            case "4.4":
-                return KITKAT_4_4;
-            default:
-                return NO_PREFERENCE;
-        }
-    }
+	private String value;
+
+	private DeviceOs(String deviceOs) {
+		this.value = deviceOs;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+
+	public DeviceOs getDeviceOs(String os) {
+		switch (os) {
+		case "4.1":
+			return JELLY_BEAN_4_1;
+		case "4.2.1":
+			return JELLY_BEAN_MR1_4_2_1;
+		case "4.2.2":
+			return JELLY_BEAN_MR1_4_2_2;
+		case "4.3":
+			return JELLY_BEAN_MR2_4_3;
+		case "4.4":
+			return KITKAT_4_4;
+		case "4.4.1":
+			return KITKAT_4_4_1;
+		case "4.4.2":
+			return KITKAT_4_4_2;
+		case "4.4.3":
+			return KITKAT_4_4_3;
+		default:
+			return NO_PREFERENCE;
+		}
+	}
 
 }
