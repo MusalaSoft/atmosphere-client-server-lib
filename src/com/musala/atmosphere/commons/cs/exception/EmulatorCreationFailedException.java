64f01cd97448482eae875880df6707afbd142765
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-06-06 12:18:40 +0000

    * Implement new AtmosphereRuntimeException class thaht extends RuntimeException and refactor all unchecked exceptions to extend that class.
    [http://jenkins.musala.com/r/8613/ $8613] see #3242
    
    git-svn-id: https://trac.musala.com/ARDC/repo@10082 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/cs/exception/EmulatorCreationFailedException.java
+++ src/com/musala/atmosphere/commons/cs/exception/EmulatorCreationFailedException.java
@@ -1,12 +1,14 @@
 package com.musala.atmosphere.commons.cs.exception;
 
+import com.musala.atmosphere.commons.exceptions.AtmosphereRuntimeException;
+
 /**
  * Thrown when creation of an emulator device fails.
  * 
  * @author yordan.petrov
  * 
  */
-public class EmulatorCreationFailedException extends RuntimeException {
+public class EmulatorCreationFailedException extends AtmosphereRuntimeException {
     // TODO: it's good to have a root RE exception in our app. Maybe it is good to introduce this intermediate level so
     // people can guard against multiple of our REs in the same time
     private static final long serialVersionUID = -2665753514724505625L;
