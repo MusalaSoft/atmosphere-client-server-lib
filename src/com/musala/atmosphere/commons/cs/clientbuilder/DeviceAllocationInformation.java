9a4bfb4e7c8de33710acc936208f40b179687721
yavor.stankov <yavor.stankov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-10-20 12:31:06 +0000

    Initial import;
    Remove PoolItem class.
    Remove PoolItem logic from the PoolManager class.
    Implement Dao's logic in the PoolManager class.
    Remove the DeviceProxy logic from the ClientRequestMonitor class.
    Make some changes in Server and ServerManager to make them capable to work with the new logic in the PoolManager.
    Refactor DeviceProxy constructor.
    Refactor some Integration and Unit tests.
    Add some java doc.
    
    [https://inttools.musala.com/reviewboard/r/333/ $333] see #4016, #4021
    
    Initial import:
    Implement removing device on agent disconnected event.
    
    [https://inttools.musala.com/reviewboard/r/344/ $344] see #3874
    
    Description
    Initial import;
    
    Implement com.musala.atmosphere.server.eventservice.event.agent package.
    Implement com.musala.atmosphere.server.eventservice.event.device package.
    Implement com.musala.atmosphere.server.eventservice.event.device.publish package.
    Implement com.musala.atmosphere.server.eventservice.event.device.allocate package.
    Implement com.musala.atmosphere.server.eventservice.event.datasource package.
    Implement com.musala.atmosphere.server.eventservice.event.datasource.create package.
    Implement com.musala.atmosphere.server.eventservice.event.datasource.create.dao package.
    Move the different events in different packages.
    
    [https://inttools.musala.com/reviewboard/r/334/ $334] see #4031
    
    
    git-svn-id: https://trac.musala.com/ARDC/repo@10726 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceAllocationInformation.java
+++ src/com/musala/atmosphere/commons/cs/clientbuilder/DeviceAllocationInformation.java
@@ -19,31 +19,49 @@ public class DeviceAllocationInformation implements Serializable {
 
     private long proxyPasskey;
 
+    private String deviceId;
+
     /**
+     * Create a instance of {@link DeviceAllocationInformation} that keeps information for a device that has been
+     * allocated.
      * 
      * @param proxyRmiId
-     *        - DeviceProxy's RMI binding identifier.
+     *        - DeviceProxy's RMI binding identifier
      * @param proxyPasskey
-     *        - passkey to be used in the DeviceProxy method invocations.
+     *        - passkey to be used in the DeviceProxy method invocations
+     * @param deviceId
+     *        - the device ID
      */
-    public DeviceAllocationInformation(String proxyRmiId, Long proxyPasskey) {
+    public DeviceAllocationInformation(String proxyRmiId, Long proxyPasskey, String deviceId) {
         this.proxyRmiId = proxyRmiId;
         this.proxyPasskey = proxyPasskey;
+        this.deviceId = deviceId;
     }
 
     /**
+     * Gets the allocated device's DeviceProxy RMI binding identifier.
      * 
-     * @return the allocated device's DeviceProxy RMI binding identifier.
+     * @return DeviceProxy RMI binding identifier.
      */
     public String getProxyRmiId() {
         return proxyRmiId;
     }
 
     /**
+     * Gets the passkey to be used in the DeviceProxy method invocations.
      * 
-     * @return the passkey to be used in the DeviceProxy method invocations.
+     * @return passkey to be used in the DeviceProxy
      */
     public long getProxyPasskey() {
         return proxyPasskey;
+    }
+
+    /**
+     * Gets the ID of the device.
+     * 
+     * @return the device ID
+     */
+    public String getDeviceId() {
+        return deviceId;
     }
 }
