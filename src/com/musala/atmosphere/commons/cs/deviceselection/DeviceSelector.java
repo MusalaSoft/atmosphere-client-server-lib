4dd75ba422b416946fea28ab1994cc016ea2b3e1
vassil.angelov <vassil.angelov@34708651-a5da-c34d-b20c-10ddab38317d>
2015-03-30 07:35:28 +0000

    Initial import:  - DeviceSelector containing the validated DeviceParameters.
    [https://inttools.musala.com/reviewboard/r/852/ $852] see #4468
    
    git-svn-id: https://trac.musala.com/ARDC/repo@11633 34708651-a5da-c34d-b20c-10ddab38317d

---
+++ src/com/musala/atmosphere/commons/cs/deviceselection/DeviceSelector.java
@@ -0,0 +1,51 @@
+package com.musala.atmosphere.commons.cs.deviceselection;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.musala.atmosphere.commons.cs.deviceselection.DeviceParameter.Visitor;
+
+/**
+ * A selector that can be used to search for specific device(s) that match the defined {@link DeviceParameter device
+ * parameters}.
+ * 
+ * @author vassil.angelov
+ *
+ */
+public class DeviceSelector implements Serializable {
+
+    private static final long serialVersionUID = -4906043158195925300L;
+
+    private HashMap<Class<? extends DeviceParameter>, DeviceParameter> deviceParameters;
+
+    /**
+     * Creates new {@link DeviceSelector} with the specified parameters.
+     * 
+     * @param parameters
+     */
+    DeviceSelector(Map<Class<? extends DeviceParameter>, DeviceParameter> parameters) {
+        deviceParameters = new HashMap<Class<? extends DeviceParameter>, DeviceParameter>(parameters);
+    }
+
+    /**
+     * Iterates over all defined parameters in this selector using the provided {@link Visitor}.
+     * 
+     * @param visitor
+     *        - the {@link Visitor} to use when iterating the parameters
+     */
+    public void visitAll(Visitor visitor) {
+        for (DeviceParameter parameter : deviceParameters.values()) {
+            parameter.accept(visitor);
+        }
+    }
+
+    /**
+     * Retrieve the defined {@link DeviceParameter device parameters} for this selector.
+     * 
+     * @return The defined {@link DeviceParameter device parameters} for this selector
+     */
+    public Map<Class<? extends DeviceParameter>, DeviceParameter> getParameters() {
+        return new HashMap<Class<? extends DeviceParameter>, DeviceParameter>(deviceParameters);
+    }
+}
