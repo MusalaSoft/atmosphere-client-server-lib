d15875e6cce6e01f08ba78a5529e5d5253e59201
Dimcho N. Nedev <dimcho.nedev@MUSALA.COM>
2017-06-29 17:03:49 +0300

    Remove the redundant serialVersionUIDs and еmpty or auto generated comments

--- src/main/java/com/musala/atmosphere/commons/cs/deviceselection/ApiLevel.java
+++ src/main/java/com/musala/atmosphere/commons/cs/deviceselection/ApiLevel.java
@@ -2,23 +2,18 @@ package com.musala.atmosphere.commons.cs.deviceselection;
 
 /**
  * Describes {@link DeviceParameter} related to the Android SDK API of a device.
- * 
+ *
  * @author vassil.angelov
  *
  */
 public abstract class ApiLevel extends SingleValueDeviceParameter<Integer> {
-
-    private static final long serialVersionUID = -7192304287970591453L;
-
     /**
      * The maximum API a device can have.
-     * 
+     *
      * @author vassil.angelov
      *
      */
     public static class Maximum extends ApiLevel {
-        private static final long serialVersionUID = 4799108544546282115L;
-
         @Override
         public void accept(Visitor visitor) {
             visitor.visit(this);
@@ -31,14 +26,11 @@ public abstract class ApiLevel extends SingleValueDeviceParameter<Integer> {
 
     /**
      * The minimum API a device can have.
-     * 
+     *
      * @author vassil.angelov
      *
      */
     public static class Minimum extends ApiLevel {
-
-        private static final long serialVersionUID = 924163466017913930L;
-
         @Override
         public void accept(Visitor visitor) {
             visitor.visit(this);
@@ -52,13 +44,11 @@ public abstract class ApiLevel extends SingleValueDeviceParameter<Integer> {
     /**
      * The targeted API a device can have. In case this parameter is present along with both (or one of)
      * {@link ApiLevel.Maximum} or {@link ApiLevel.Minimum}, the target is considered with higher priority.
-     * 
+     *
      * @author vassil.angelov
      *
      */
     public static class Target extends ApiLevel {
-        private static final long serialVersionUID = 7748774722618853196L;
-
         @Override
         public void accept(Visitor visitor) {
             visitor.visit(this);
